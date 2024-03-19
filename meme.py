a8c5eac3749f58072fb96fa04fb2300f6fc12613
ramon <ramonpolanski@gmail.com>
2024-03-19 11:55:46 +0100

    memes.py working

---
+++ meme.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+# Open the first image
+image1 = Image.open("51KT0rBrvwL._AC_UF894,1000_QL80_.jpg")
+image1.show()
+
+# Open the second image
+image2 = Image.open("D-pMpjlX4AIhsVw.jpg")
+image2.show()
